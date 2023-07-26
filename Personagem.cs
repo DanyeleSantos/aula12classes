adfcb36735af80cb418e38171b56454acd1051ae
DanyeleSantos <135742938+DanyeleSantos@users.noreply.github.com>
2023-07-26 13:35:28 -0300

    Add files via upload

---
+++ Personagem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula12Classes
+{
+    internal class Personagem
+    {
+        // VARIAVEÍS QUE PASSA A SE CHAMAR (ATRIBUTOS)
+        public string nome;
+        public string raca;
+        public int poder;
+
+        // FUNÇÕES QUE PASSA A SE CHAMAR (MÉTODOS)
+
+        public void mostra()
+        {
+            Console.WriteLine($"Seu personagem é {raca} o nome é {nome} e o pode é {poder}");
+        }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿
+using Aula12Classes;
+
+Console.WriteLine("Hello, Word!");
+
+Personagem guerreiro = new Personagem();
+guerreiro.nome = "Conrado";
+guerreiro.raca = "Humano";
+guerreiro.poder = 100;
+
+guerreiro.mostra();
+
+Personagem mago = new Personagem();
+mago.raca = "elfo";
+mago.poder = 150;
+mago.nome = "Harry";
+
+mago.mostra();
+
+Personagem bruxo = mago;
+bruxo.nome = "herminione";
+bruxo.mostra();
+
+
+Personagem boneca  = new Personagem();
+boneca.raca = "barbie";
+boneca.poder = 300;
+boneca.nome = "Margot";
+
+boneca.mostra();
+
+
+
+
+
+
+
